110b40bd54a48a20304083425ac379247e27e51c
arbenger <arbenger@git.com>
2021-06-19 09:49:13 +0800

    updated BookSummary

--- components/pages/book/Summary/index.tsx
+++ components/pages/book/Summary/index.tsx
@@ -3,8 +3,12 @@ import {
    PageContainer,
    PageTitle,
 } from 'components/layouts/Page/styled';
+import { Grid } from '@material-ui/core';
 import { useRouter } from 'next/router';
 import { Fragment } from 'react';
+
+import FeatureOne from './components/FeatureOne';
+import FeatureTwo from './components/FeatureTwo';
 
 export default function BookSummary() {
    const router = useRouter();
@@ -14,6 +18,16 @@ export default function BookSummary() {
          <WaveBackground />
          <PageContainer maxWidth="lg">
             <PageTitle variant="h4">Book Summary</PageTitle>
+
+            <Grid container spacing={2}>
+               <Grid item xs={12} md={4}>
+                  <FeatureOne />
+               </Grid>
+
+               <Grid item xs={12} md={8}>
+                  <FeatureTwo />
+               </Grid>
+            </Grid>
          </PageContainer>
       </Fragment>
    );
