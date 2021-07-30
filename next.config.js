d877aeb0f77bc07710d8dd06b4b1392605992304
arbenger <arbenger@git.com>
2021-07-30 22:40:51 +0800

    added base code

---
+++ next.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+   images: {
+      domains: [
+         'lh3.googleusercontent.com',
+         'firebasestorage.googleapis.com',
+         'localhost',
+      ],
+   },
+   env: {
+      apiKey: 'AIzaSyBFeM2425df_f9d24TDxJqzRy4Lp8Cgo0w',
+      authDomain: 'lr-hub-sjdmnts.firebaseapp.com',
+      projectId: 'lr-hub-sjdmnts',
+      storageBucket: 'lr-hub-sjdmnts.appspot.com',
+      messagingSenderId: '352114457828',
+      appId: '1:352114457828:web:d73022558aae6c5b8a80f1',
+      measurementId: 'G-V7YGZ03RCY',
+      secretAPIAccessKey:
+         'Yin and yang, male and female, strong and weak, rigid and tender, heaven and earth, light and darkness, thunder and lightning, cold and warmth, good and evil... the interplay of opposite principles constitutes the universe',
+      FIRESTORE_EMULATOR_HOST: 'localhost:8080',
+      FIREBASE_AUTH_EMULATOR_HOST: 'localhost:9099',
+   },
+};
